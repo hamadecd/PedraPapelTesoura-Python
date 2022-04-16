94edb891c8cb921bba72c03f3a64fc34206c5ec9
William <williamhss90@gmail.com>
2022-04-16 09:46:46 -0300

    repetir o jogo adicionado

--- ppt.py
+++ ppt.py
@@ -1,41 +1,52 @@
 import random
 
-# Array que contém as 3 opções do jogo
-computador = ('pedra', 'papel', 'tesoura')
+while True:
+    pass
+    # Array que contém as 3 opções do jogo
+    computador = ('pedra', 'papel', 'tesoura')
 
-# recebe o valor digitado pelo jogador
-jogador = input('Digite Pedra, Papel, Tesoura\n')
+    # recebe o valor digitado pelo jogador
+    jogador = input('Digite Pedra, Papel, Tesoura\n').lower()
 
-# agora computador recebe somente um valor aleatório, escolhido dentre seu conjunto de valores
-computador = random.choice(computador)
+    # agora computador recebe somente um valor aleatório, escolhido dentre seu conjunto de valores
+    computador = random.choice(computador)
 
-# nome de variável em Python normalmente se usa o caractere underline para separar as palavras (nome disso é Snake_Case, temos o CamelCase entre otros
-# mas por boas práticas de desenvolvimento o mais comum é utilizar o snake_case)
-informa_Resultado = ''
+    # nome de variável em Python normalmente se usa o caractere underline para separar as palavras (nome disso é Snake_Case, temos o CamelCase entre otros
+    # mas por boas práticas de desenvolvimento o mais comum é utilizar o snake_case)
+    informa_Resultado = ''
 
-if jogador == computador:
-    informa_Resultado = 'Você Empatou'
-elif (jogador == 'pedra'):
-    if computador == 'tesoura' :
-        informa_Resultado = 'Você Venceu'
-    elif computador == 'papel' :
-        informa_Resultado = 'Você Perdeu'
-elif (jogador == 'papel'):
-    if computador == 'pedra' :
-        informa_Resultado = 'Você Venceu'
-    elif computador == 'tesoura' :
-        informa_Resultado = 'Você Perdeu'
-elif (jogador == 'tesoura'):
-    if computador == 'papel' :
-        informa_Resultado = 'Você Venceu'
-    elif computador == 'pedra' :
-        informa_Resultado = 'Você Perdeu'
-else:
-    informa_Resultado = 'Não foi informada uma palavra válida'
+    if jogador == computador:
+        informa_Resultado = 'Você Empatou'
+    elif (jogador == 'pedra'):
+        if computador == 'tesoura' :
+            informa_Resultado = 'Você Venceu'
+        elif computador == 'papel' :
+            informa_Resultado = 'Você Perdeu'
+    elif (jogador == 'papel'):
+        if computador == 'pedra' :
+            informa_Resultado = 'Você Venceu'
+        elif computador == 'tesoura' :
+            informa_Resultado = 'Você Perdeu'
+    elif (jogador == 'tesoura'):
+        if computador == 'papel' :
+            informa_Resultado = 'Você Venceu'
+        elif computador == 'pedra' :
+            informa_Resultado = 'Você Perdeu'
+    else:
+        informa_Resultado = 'Não foi informada uma palavra válida'
 
-# Antes de exibir o resultado, o jogo mostra cada opção os jogadores escolheram
-print('Jogador escolheu: ' + jogador + ' \nComputador escolheu: ' + computador)
+    # Antes de exibir o resultado, o jogo mostra cada opção os jogadores escolheram
+    print('Jogador escolheu: ' + jogador + ' \nComputador escolheu: ' + computador)
 
-print(informa_Resultado)
+    print(informa_Resultado)
 
-#lembrar sempre de ter um código limpo e de fácil entendimento. Evitar de deixar sem espaçamento entre as linhas.
+    #lembrar sempre de ter um código limpo e de fácil entendimento. Evitar de deixar sem espaçamento entre as linhas.
+
+    #variavel para repetir o game
+    repetir = input("Jogar Novamente? (s/n)").lower()
+
+    #condição para repetir o game
+    if repetir != 's':
+        break
+
+print("Jogo Finalizado!")
