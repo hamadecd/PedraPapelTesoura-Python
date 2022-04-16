59c982eb70c67a2ac878aec4f84b12f2c8757377
Samir <samircd9@gmail.com>
2022-04-16 00:27:10 -0300

    atualiza comentários no código

--- ppt.py
+++ ppt.py
@@ -1,31 +1,35 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 import random
-computador = ("pedra", "papel", "tesoura")
-valor = input("Digite Pedra, Papel, Tesoura\n")
+# variável que contém as 3 opções do jogo
+computador = ('pedra', 'papel', 'tesoura')
+# recebe o valor digitado pelo jogador
+jogador = input('Digite Pedra, Papel, Tesoura\n')
+# agora computador recebe somente um valor aleatório, escolhido dentre seu conjunto de valores
 computador = random.choice(computador)
-f = ""
+# nome de variável em Python normalmente se usa o caractere underline para separar as palavras
+informa_Resultado = ''
 
-if valor == computador:
-    f = "Você Empatou"
-elif (valor == "pedra"):
-    if computador == "tesoura" :
-        f = "Você Venceu"
-    elif computador == "papel" :
-        f = "Você Perdeu"
-elif (valor == "papel"):
-    if computador == "pedra" :
-        f = "Você Venceu"
-    elif computador == "tesoura" :
-        f = "Você Perdeu"
-elif (valor == "tesoura"):
-    if computador == "papel" :
-        f = "Você Venceu"
-    elif computador == "pedra" :
-        f = "Você Perdeu"
+if jogador == computador:
+    informa_Resultado = 'Você Empatou'
+elif (jogador == 'pedra'):
+    if computador == 'tesoura' :
+        informa_Resultado = 'Você Venceu'
+    elif computador == 'papel' :
+        informa_Resultado = 'Você Perdeu'
+elif (jogador == 'papel'):
+    if computador == 'pedra' :
+        informa_Resultado = 'Você Venceu'
+    elif computador == 'tesoura' :
+        informa_Resultado = 'Você Perdeu'
+elif (jogador == 'tesoura'):
+    if computador == 'papel' :
+        informa_Resultado = 'Você Venceu'
+    elif computador == 'pedra' :
+        informa_Resultado = 'Você Perdeu'
 else:
-    f = "Palavra não encontrada"
+    informa_Resultado = 'Não foi informada uma palavra válida'
+# Antes de exibir o resultado, o jogo mostra cada opção os jogadores escolheram
+print('Jogador escolheu: ' + jogador + ' \nComputador escolheu: ' + computador)
 
-print("Jogador escolheu: " + valor + " \nComputador escolheu: "+computador)
-
-print(f)
+print(informa_Resultado)
